e7fb0834191ad62e1370c7afb2178a76eab84732
momo <mmko5110@gmail.com>
2020-01-12 17:01:17 +0900

    first commit

---
+++ FirestoreTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirestoreTest
+//
+//  Created by 折田桃子 on 2020/01/12.
+//  Copyright © 2020 Momoko Orita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
